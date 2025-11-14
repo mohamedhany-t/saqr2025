e8c3f5349c1f3328c7909d28ff9478950eabc18d
Mohamed Hany <m.hanau123456@gmail.com>
2025-11-14 18:20:44 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9762882993-dc7cd",
+  "appId": "1:845078294314:web:0a724ebcea20963cb87853",
+  "apiKey": "AIzaSyArOuBdWNCL56P6H0zyxijkCbhM_Bqoaro",
+  "authDomain": "studio-9762882993-dc7cd.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "845078294314"
+};
