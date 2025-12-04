c9b2647f4e6d7130023d0c50258ef2ef19e6ab35
Mohamed Hany <m.hanau123456@gmail.com>
2025-12-04 01:27:10 +0000

    اهو Default
    studio-9762882993-dc7cd.firebasestorage.app

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,8 @@
+
 export const firebaseConfig = {
   "projectId": "studio-9762882993-dc7cd",
   "appId": "1:845078294314:web:0a724ebcea20963cb87853",
-  "storageBucket": "studio-9762882993-dc7cd.firebaseapp.com",
+  "storageBucket": "studio-9762882993-dc7cd.firebasestorage.app",
   "apiKey": "AIzaSyArOuBdWNCL56P6H0zyxijkCbhM_Bqoaro",
   "authDomain": "studio-9762882993-dc7cd.firebaseapp.com",
   "measurementId": "",
